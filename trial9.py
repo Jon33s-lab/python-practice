713e6796d4cbf37b95738aa64f879681c309117f
Jon33s-lab <2002434@students.kcau.ac.ke>
2022-08-24 09:51:49 +0300

    importing turtle library and drawing a square using turtle

---
+++ trial9.py
@@ -0,0 +1,11 @@
+import turtle#allows us to use the turtle library
+wn=turtle.Screen()#creates a graphic window
+Trevour=turtle.Turtle()#creates a turtle named Trevour
+Trevour.forward(150)
+Trevour.left(90)
+Trevour.forward(150)
+Trevour.left(90)
+Trevour.forward(150)
+Trevour.left(90)
+Trevour.forward(150)
+
